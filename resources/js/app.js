e12fa2f967c0720ae359b8e1d8e5cd436c3b5858
Damian Kajewski <orzechovaty@gmail.com>
2020-05-09 23:07:57 +0200

    SPA with vue and vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import Routes from './routes';
+import App from './views/App';
+
+const app = new Vue({
+    el: '#app',
+    router: Routes,
+    render: h => h(App),
+});
+
+export default app;
---
+++ resources/js/components/basic/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="content">
+        <h1>Home content</h1>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
